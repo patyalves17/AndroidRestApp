060a78ffa86a7f9c082a74f0eb95ada0e6b6b872
patyalves17 <Patyptrc2915>
2017-06-21 21:51:13 -0300

    .

--- app/src/main/java/com/fiap/patyalves/androidrestapp/adapter/AndroidAdapter.java
+++ app/src/main/java/com/fiap/patyalves/androidrestapp/adapter/AndroidAdapter.java
@@ -1,7 +1,9 @@
 package com.fiap.patyalves.androidrestapp.adapter;
 
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -9,12 +11,14 @@ import android.widget.TextView;
 
 import com.fiap.patyalves.androidrestapp.R;
 import com.fiap.patyalves.androidrestapp.modelo.Android;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
 public class AndroidAdapter extends RecyclerView.Adapter<AndroidAdapter.AndroidViewHolder> {
 
     private List<Android> androids;
+    private Context context;
 
     public AndroidAdapter(List<Android> androids){
         this.androids=androids;
@@ -23,12 +27,16 @@ public class AndroidAdapter extends RecyclerView.Adapter<AndroidAdapter.AndroidV
 
     @Override
     public AndroidViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater=LayoutInflater.from(parent.getContext());
+        View meuLayout = inflater.inflate(R.layout.android_row, parent,false);
+        return new AndroidViewHolder(meuLayout);
     }
 
     @Override
     public void onBindViewHolder(AndroidViewHolder holder, int position) {
-
+        holder.tvTitulo.setText(androids.get(position).getNome());
+        holder.tvSubTitulo.setText(androids.get(position).getVersao());
+        Picasso.with(holder.itemView.getContext()).load(androids.get(position).getUrlImagem()).into(holder.ivLogo);
     }
 
     @Override
